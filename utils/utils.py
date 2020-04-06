d5b32c46dbc80da8ecd374d966d7ae8ea9c2d418
Daniel Jeffrey Wu <danjwu@cees-mazama-gpu.stanford.edu>
2020-04-05 17:16:11 -0700

    4/5/20 Checkpoint

--- utils/utils.py
+++ utils/utils.py
@@ -36,15 +36,14 @@ def multivariate_data_balanced(dataset, history_size,
         quake = dataset[q_idx]
         quake_class = quake_classes[q_idx] #class for quake
         quake_wt = int(num_copies[int(quake_class)])
-        for _ in range(quake_wt):
-            for i in range(start_index, end_index):
-                indices = range(i-history_size, i, step)
-                data.append(quake[indices])
+        for i in range(start_index, end_index):
+            indices = range(i-history_size, i, step)
+            data.extend([quake[indices]]*quake_wt)
 
-                if single_step:
-                    labels.append(quake[i+target_size])
-                else:
-                    labels.append(quake[i:i+target_size])
+            if single_step:
+                labels.extend([quake[i+target_size]]*quake_wt)
+            else:
+                labels.extend([quake[i:i+target_size]]*quake_wt)
 
     return np.array(data), np.array(labels)
 
@@ -94,3 +93,29 @@ def recover_quake(y_pred, y, means, norms, num_quakes):
 
     return y, y_pred, np.abs((y - y_pred).reshape((-1,)))
 
+def show_plot(plot_data, delta, title):
+    labels = ['History', 'True Future', 'Model Prediction']
+    marker = ['.-', 'rx', 'go']
+    time_steps = create_time_steps(plot_data[0].shape[0])
+    if delta:
+        future = delta
+    else:
+        future = 0
+
+    plt.title(title)
+    for i, x in enumerate(plot_data):
+        if i:
+            plt.plot(future, plot_data[i], marker[i], markersize=10,
+               label=labels[i])
+        else:
+            plt.plot(time_steps, plot_data[i].flatten(), marker[i], label=labels[i])
+    plt.legend()
+    plt.xlim([time_steps[0], (future+5)*2])
+    plt.xlabel('Time-Step')
+    return plt
+
+def create_time_steps(length):
+    time_steps = []
+    for i in range(-length, 0, 1):
+        time_steps.append(i)
+    return time_steps
